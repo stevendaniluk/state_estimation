7126dcc7d7f1c6208dc107a7758a205b6d7f89ba
stevendaniluk <stevendaniluk8@gmail.com>
2019-12-31 10:38:04 -0800

    Measurement definitions for an IMU with orientation

--- include/state_estimation/definitions/common_measurements.h
+++ include/state_estimation/definitions/common_measurements.h
@@ -17,6 +17,37 @@ namespace imu {
     static const int DIMS = 6;
 }
 
+// Measurement vector indices for an IMU with RPY orientation
+namespace imu_rpy {
+    static const int AX = 0;
+    static const int AY = 1;
+    static const int AZ = 2;
+    static const int VPHI = 3;
+    static const int VTHETA = 4;
+    static const int VPSI = 5;
+    static const int PHI = 6;
+    static const int THETA = 7;
+    static const int PSI = 8;
+
+    static const int DIMS = 9;
+}
+
+// Measurement vector indices for an IMU with quaternion orientation
+namespace imu_quat {
+    static const int AX = 0;
+    static const int AY = 1;
+    static const int AZ = 2;
+    static const int VPHI = 3;
+    static const int VTHETA = 4;
+    static const int VPSI = 5;
+    static const int W = 6;
+    static const int X = 7;
+    static const int Y = 8;
+    static const int Z = 9;
+
+    static const int DIMS = 10;
+}
+
 // Measurement vector indices for non holonomic wheel odometry measurements
 namespace nh_odom {
     static const int VX = 0;
