1d49a9dd0cb9d5e3e686d3305d6715dd222b831f
stevendaniluk <stevendaniluk8@gmail.com>
2020-10-24 12:51:25 -0700

    Fix logging bug for printing transpose of column vectors

--- include/state_estimation/utilities/logging.hpp
+++ include/state_estimation/utilities/logging.hpp
@@ -6,8 +6,11 @@ namespace state_estimation {
 template <typename Real, int M, int N>
 std::string printMatrix(const Eigen::Matrix<Real, M, N>& mat) {
     std::ostringstream stream;
-    stream << ((mat.cols() > 1) ? mat : mat.transpose())
-                  .format(Eigen::IOFormat(6, 0, ", ", "\n", "[", "]"));
+    if (mat.cols() == 1) {
+        stream << mat.transpose().format(Eigen::IOFormat(6, 0, ", ", "\n", "[", "]"));
+    }else {
+        stream << mat.format(Eigen::IOFormat(6, 0, ", ", "\n", "[", "]"));
+    }
     return stream.str();
 }
 
